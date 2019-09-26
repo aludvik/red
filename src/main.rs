298cb9a071d01f3275df86d2c1872a04dcb9b101
Adam M Ludvik <adam.m.ludvik@gmail.com>
2019-09-25 21:33:56 -0700

    Finish initial features
    Signed-off-by: Adam M Ludvik <adam.m.ludvik@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 /*
 [x] Create a new file and open it
 [x] Open an existing file
-[ ] Navigate to a location in an open file
-[ ] Insert a character at the current location
-[ ] Delete a character at the current location
-[ ] Write an open file out
+[x] Navigate to a location in an open file
+[x] Insert a character at the current location
+[x] Delete a character at the current location
+[x] Write an open file out
 */
 
 extern crate termion;
@@ -18,7 +18,8 @@ use termion::{
   input::TermRead,
 };
 
-type Buffer = Vec<String>;
+type Line = String;
+type Buffer = Vec<Line>;
 
 fn open_file(path: &str) -> io::Result<fs::File> {
   fs::OpenOptions::new().read(true).write(true).create(true).open(path)
@@ -37,65 +38,144 @@ fn write_file(path: &str, buf: &Buffer) -> io::Result<()> {
   file.flush()
 }
 
-type Cursor = (usize, usize);
+struct Cursor {
+  col: usize,
+  row: usize,
+  left: usize,
+  top: usize,
+}
 
-fn insert_at(ch: u8, cur: &mut Cursor, buf: &mut Buffer) {
+fn insert_at(ch: char, cur: &mut Cursor, buf: &mut Buffer) {
+  buf[cur.row].insert(cur.col, ch)
 }
 
 fn delete_at(cur: &mut Cursor, buf: &mut Buffer) {
+  buf[cur.row].remove(cur.col);
 }
 
 type Screen = termion::raw::RawTerminal<io::Stdout>;
 
-fn write_buffer_to_screen(
+struct Size {
+  cols: usize,
+  rows: usize,
+}
+
+fn write_line_to_screen(
   scr: &mut Screen,
   cur: &Cursor,
-  buf: &mut Buffer,
+  line: &Line,
+  size: &Size,
 ) -> io::Result<()> {
+  let bytes = line.as_bytes();
+  for i in cur.left..(cur.left + size.cols) {
+    write!(scr, "{}", bytes[i])?;
+  }
   Ok(())
 }
 
+fn write_buffer_to_screen(
+  scr: &mut Screen,
+  cur: &Cursor,
+  buf: &Buffer,
+  size: &Size,
+) -> io::Result<()> {
+  for i in cur.top..(cur.top + size.rows) {
+    write_line_to_screen(scr, cur, &buf[i], size)?;
+  }
+  write!(scr, "{}", termion::cursor::Goto(
+    (cur.row - cur.top + 1) as u16,
+    (cur.col - cur.left + 1) as u16,
+  ))?;
+  scr.flush()
+}
+
 fn clear_screen(scr: &mut Screen) -> io::Result<()> {
-  Ok(())
+  write!(scr, "{}{}", termion::clear::All, termion::cursor::Goto(1, 1))?;
+  scr.flush()
 }
 
 fn update_screen(
   scr: &mut Screen,
   cur: &Cursor,
-  buf: &mut Buffer,
+  buf: &Buffer,
+  size: &Size,
 ) -> io::Result<()> {
   clear_screen(scr)?;
-  write_buffer_to_screen(scr, cur, buf)
+  write_buffer_to_screen(scr, cur, buf, size)
 }
 
 fn init_screen() -> io::Result<Screen> {
   io::stdout().into_raw_mode()
 }
 
-enum Dir { Left, Right, Up, Down }
-
-fn update_cursor(cur: &mut Cursor, buf: &Buffer, dir: Dir) {
-  match dir {
-    Dir::Left => (),
-    Dir::Right => (),
-    Dir::Up => (),
-    Dir::Down => (),
+fn align_cursor(cur: &mut Cursor, size: &Size) {
+  if cur.col < cur.left {
+    cur.left = cur.col;
+  }
+  if cur.col > cur.left + size.cols - 1 {
+    cur.left = cur.col - size.cols + 1;
+  }
+  if cur.row < cur.top {
+    cur.top = cur.row;
+  }
+  if cur.row > cur.top + size.rows - 1 {
+    cur.top = cur.row - size.rows + 1;
   }
 }
+
+fn move_cursor_left(cur: &mut Cursor, _buf: &Buffer, size: &Size) {
+  if cur.col > 0 {
+    cur.col -= 1;
+  }
+  align_cursor(cur, size);
+}
+
+fn move_cursor_right(cur: &mut Cursor, buf: &Buffer, size: &Size) {
+  if cur.col < buf[cur.row].len() - 1 {
+    cur.col += 1;
+  }
+  align_cursor(cur, size);
+}
+
+fn truncate_cursor_to_line(cur: &mut Cursor, buf: &Buffer, size: &Size) {
+  if cur.col >= buf[cur.row].len() {
+    cur.col = buf[cur.row].len() - 1;
+  }
+  align_cursor(cur, size);
+}
+
+fn move_cursor_up(cur: &mut Cursor, buf: &Buffer, size: &Size) {
+  if cur.row > 0 {
+    cur.row -= 1;
+  }
+  truncate_cursor_to_line(cur, buf, size);
+  align_cursor(cur, size);
+}
+
+fn move_cursor_down(cur: &mut Cursor, buf: &Buffer, size: &Size) {
+  if cur.row < buf.len() - 1 {
+    cur.row += 1;
+  }
+  truncate_cursor_to_line(cur, buf, size);
+  align_cursor(cur, size);
+}
+
 type Key = termion::event::Key;
 
 fn edit_buffer(buf: &mut Buffer) -> io::Result<()> {
   let mut scr = init_screen()?;
-  let mut cur = (0, 0);
+  let mut cur = Cursor{col: 0, row: 0, left: 0, top: 0};
   for res in io::stdin().keys() {
     let key = res?;
-    update_screen(&mut scr, &cur, buf)?;
+    let size = termion::terminal_size()
+      .map(|(rows, cols)| Size{rows: rows as usize, cols: cols as usize})?;
+    update_screen(&mut scr, &cur, buf, &size)?;
     match key {
-      Key::Left => update_cursor(&mut cur, buf, Dir::Left),
-      Key::Right => update_cursor(&mut cur, buf, Dir::Right),
-      Key::Up => update_cursor(&mut cur, buf, Dir::Up),
-      Key::Down => update_cursor(&mut cur, buf, Dir::Down),
-      Key::Char(ch) => insert_at(ch as u8, &mut cur, buf),
+      Key::Left => move_cursor_left(&mut cur, buf, &size),
+      Key::Right => move_cursor_right(&mut cur, buf, &size),
+      Key::Up => move_cursor_up(&mut cur, buf, &size),
+      Key::Down => move_cursor_down(&mut cur, buf, &size),
+      Key::Char(ch) => insert_at(ch, &mut cur, buf),
       Key::Backspace => delete_at(&mut cur, buf),
       _ => break,
     }
